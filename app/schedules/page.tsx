b5b4ef2f33bf70d0f76f6d223c29ff25da8c2c1e
Josphat84 <abnjosphat@gmail.com>
2026-02-17 00:20:16 +0200

    //

--- app/schedules/page.tsx
+++ app/schedules/page.tsx
@@ -1,1 +1,12 @@
 //Code to come here
+
+"use client";
+
+export default function MaintenanceCompliancePage() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Hello World</h1>
+      <p>Temporary placeholder – build will pass.</p>
+    </div>
+  );
+}
